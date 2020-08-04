fd592b8fd6161a1d69739004666366697f3f70a7
ozzy205 <ozzwald.barrion0801@gmail.com>
2020-08-04 16:08:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-qtuhbb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-qtuhbb)
---
+++ src/app/product-alers/product-alers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alers',
+  templateUrl: './product-alers.component.html',
+  styleUrls: ['./product-alers.component.css']
+})
+export class ProductAlersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
